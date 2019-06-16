ad74c13eced2f2fcafefff41718ab868bc3a55df
rtr-x8 <ryu.2.24@hotmail.co.jp>
2019-06-16 06:30:30 +0000

    コマンド追加

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,8 +5,18 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use Artisan;
+
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:refresh');
+    }
+
     /**
      * A basic test example.
      *
