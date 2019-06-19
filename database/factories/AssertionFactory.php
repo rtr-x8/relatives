c2830d92ecf58dcdfb60ecbb2e679e7b5b09ed3a
rtr-x8 <ryu.2.24@hotmail.co.jp>
2019-06-19 12:54:29 +0000

    Set seeder and factory

--- database/factories/AssertionFactory.php
+++ database/factories/AssertionFactory.php
@@ -5,12 +5,12 @@
 use App\Assertion;
 use Faker\Generator as Faker;
 
-$factory->define(Assertion::class, function (Faker $faker, $attributes) {
-    $user_id = isset($attributes['user_id']) ?
-        $attributes['user_id'] : factory(App\User::class)->create()->id;
+$factory->define(Assertion::class, function (Faker $faker) {
     return [
         'body' => $faker->text,
         'provider' => "",
-        'user_id' => $user_id,
+        'user_id' => function() {
+            return factory(App\User::class)->create()->id;
+        },
     ];
 });
