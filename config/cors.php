b4fa3024790896b30124e0fde048cec4111bcff8
rtr-x8 <ryu.2.24@hotmail.co.jp>
2019-06-23 07:55:16 +0000

    set allowed origins

--- config/cors.php
+++ config/cors.php
@@ -1,5 +1,9 @@
 <?php
 
+$allowed_origin01 = env('ALLOWED_ORIGIN01', "http://localhost:3000");
+$allowed_origin02 = env('ALLOWED_ORIGIN02', "http://localhost:3000");
+$allowed_origin03 = env('ALLOWED_ORIGIN03', "http://localhost:3000");
+
 return [
 
     /*
@@ -13,7 +17,11 @@ return [
     */
 
     'supportsCredentials' => false,
-    'allowedOrigins' => [ 'http://localhost:3000' ],
+    'allowedOrigins' => [
+        $allowed_origin01,
+        $allowed_origin02,
+        $allowed_origin03
+    ],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => [],
     'allowedMethods' => ['*'],
